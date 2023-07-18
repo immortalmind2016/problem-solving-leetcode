2649afcbfec45764ac0cd62d3cf7e67d6cb2835d
mohamed salah <mohamedsalah.software@gmail.com>
2023-07-19 01:04:28 +0300

    feat: add another solution for lenght of longest sub string

--- string/length-longest-sub-string.ts
+++ string/length-longest-sub-string.ts
@@ -25,4 +25,31 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 
-console.log(lengthOfLongestSubstring("ab"))
+var lengthOfLongestSubstring2 = function(s) {
+  let left = 0;
+  let right = 0;
+  let max = 0;
+  let map={}
+
+  while(right<s.length){
+    map[s[right]]=(map[s[right]]||0)+1
+    
+    while(map[s[right]]>1){
+    
+      if(map[s[right]]>1){
+        map[s[right]]--;
+      }else{
+       delete map[s[right]]
+      }
+      left++;
+    }
+
+    max=Math.max(max,right-left+1)
+
+    right++;
+  }
+
+  return max;
+};
+
+console.log(lengthOfLongestSubstring2("aaaa"))

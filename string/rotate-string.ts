bf8c64a89c12c5e52c7bee483e7792878c582443
mohamed salah <mohamedsalah.software@gmail.com>
2023-07-18 22:17:12 +0300

    feat: rotate-string

---
+++ string/rotate-string.ts
@@ -0,0 +1,45 @@
+function rotateString(s: string, goal: string): boolean {
+  if(s.length!==goal.length){
+      return false
+  }
+
+  if(s.length==0&&goal.length==0){
+      return true
+  }
+  let i=0;
+  let arr=s.split("")
+  while(i<s.length-1){
+      let lastElement=arr[arr.length-1]
+      let beforeLastElement=arr.splice(0,arr.length-1);
+      arr=[lastElement,...beforeLastElement]
+      if(arr.join("")==goal){
+          return true
+      }
+      i++;
+  }
+
+  return rotateString3(s,goal)
+};
+
+function rotateString2(s: string, goal: string): boolean {
+
+  let i=0;
+
+  //O(N)
+  let indexOf=s.indexOf(goal[0]);
+
+  //O(N)
+  while(i<s.length){
+      if(s[(indexOf+i) % s.length]!==goal[i]){
+          return false
+      }
+      i++;
+  }
+
+  return true
+};
+
+function rotateString3(s: string, goal: string): boolean {
+  //O(N)
+  return (s+s).includes(goal);
+};

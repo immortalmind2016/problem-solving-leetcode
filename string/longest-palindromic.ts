7404a8d09a961fe4dd5e2573cff181c6842a9be5
immortalmind2016 <immortal.mind2016@gmail.com>
2023-07-21 01:20:49 +0300

    chore: add another approach for longest palindormic

--- string/longest-palindromic.ts
+++ string/longest-palindromic.ts
@@ -21,18 +21,10 @@ const aroundCenter:(createIndex:number,s:string,isOdd?:boolean)=>string=(centerI
   return str
 }
 
-/*
-b a b a d
-  c
-
-
-
-*/
-
 
 function longestPalindrome(s: string): string {
 
-  let globalMax=0
+
   let globalStr=""
 
   for(let i=0;i<s.length;i++){
@@ -59,4 +51,63 @@ function longestPalindrome(s: string): string {
 
 };
 
-console.log(longestPalindrome("aa"))
+
+//# Another solution
+/*
+
+babad
+s
+ e
+
+#i=0 , s=-1  e=1
+ s = -1
+ e = 1
+#i=1 , s= 0 e=2
+  s=-1
+  e=3
+
+*/
+
+
+const aroundCenter2:(start:number,end:number,s:string)=>{start,end}=(start:number,end:number,s:string):{start,end}=>{
+
+
+  while(start>=0&&end<s.length&&s[start]==s[end]){
+     start--
+     end++
+  }
+
+  return {start:start+1,end}
+}
+
+
+function longestPalindrome2(s: string): string {
+
+
+  let globalMax={start:0,end:1}
+
+  for(let i=0;i<s.length;i++){
+
+      //odd length
+      let odd=aroundCenter2(i-1,i+1,s)
+
+      //even length
+      let even=aroundCenter2(i,i+1,s)
+
+      let max=even?.end-even.start>odd.end-odd.start?even:odd
+
+      if(max.end-max.start>globalMax.end-globalMax.start){
+        globalMax=max
+      }
+
+    }
+
+    let {start,end}=globalMax
+
+    return s.slice(start,end)
+
+};
+
+
+
+console.log(longestPalindrome2("babad"))

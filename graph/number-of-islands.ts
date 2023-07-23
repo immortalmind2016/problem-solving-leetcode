37416fef740682e98efe577fd4ac2d9795da46d5
immortalmind2016 <immortal.mind2016@gmail.com>
2023-07-24 01:36:01 +0300

    chore: finalize the logic of number of islands

--- graph/number-of-islands.ts
+++ graph/number-of-islands.ts
@@ -10,11 +10,22 @@ grid = [
 
 */
 
-const numberOfIslands=(grid:number[][])=>{
+const numberOfIslands=(grid:string[][])=>{
   
   let number=0
+  //visited => [0][0]=true, [0,1]=false and so on.
+  //So we will have the same dim as grid one 
+
+  let visited=grid.map((row)=>row.map(()=>false))
 
   //iterate over islands and do DFS
+  for(let i=0;i<grid.length;i++){
+    for(let j=0;j<grid[i].length;j++){
+      if(DFS(i,j,visited)){
+        number++;
+      }
+    }
+  }
 
   return number
 }

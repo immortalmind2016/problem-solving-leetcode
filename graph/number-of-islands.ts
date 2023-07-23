3b2d5191a1288381bca46b0ddf2bb3f22c4f924d
immortalmind2016 <immortal.mind2016@gmail.com>
2023-07-24 01:31:24 +0300

    chore: init number of islands problem

--- graph/number-of-islands.ts
+++ graph/number-of-islands.ts
@@ -8,34 +8,4 @@ grid = [
   ["0","0","0","0","0"]
 ]
 
-Go to 
-[0][0] push up,left,right,down if(1) => [0,1] [1,0]
-[[0,1],[1,0]] 
-Go to [1,0]
-push up,left,right,down if(1) && if not visited =>
-Stack [[0,1],[1,]]
-
-for i in grid
-  for j in grid
-    island=0
-    stack=[[i,j]]
-    while(stack.length){
-      let currentNode=stack.pop()
-
-      if(visited[i][j]){
-         continue
-      }
-
-      if not ground skip
-
-      const adj=ADJ
-      stack.push()
-
-      islandSize++
-    }
-
-
-
-
-
 */
